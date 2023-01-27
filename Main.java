5692cca9df39350211fee2754ed977f300bb05c8
chakra473 <91408040+chakra473@users.noreply.github.com>
2023-01-27 17:35:34 +0530

    added starPattren and main method
    added ternery operator with example and star pattern class

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        StarPattern starPattern=new StarPattern();
+        starPattern.star();
+        int result = 0;
+                int a= 1;
+                        int b=5;
+        result= a<=b ? a:b;
+        System.out.println(result);
+        boolean app_working = true;
+        String res;
+        res = (app_working == true) ? "app is working fine " : "app is not working fine";
+        System.out.println(res);
+    }
+}
---
+++ StarPattern.java
@@ -0,0 +1,18 @@
+public class StarPattern {
+    public void star() {
+        int i = 6, row = 6, j = 6;
+        for (i = 0; i < row; i++) {
+            {
+                for (j = 0; j <= i; j++) {
+                    System.out.println("* ");
+                    System.out.println();
+                }
+
+            }
+
+
+        }
+    }
+}
+
+
